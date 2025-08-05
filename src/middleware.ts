9e5898990077869b5ba9a12564be10553e5b8f82
vishal kumar <149081237+sawvishal@users.noreply.github.com>
2025-08-06 03:02:47 +0530

    worked on login auth

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse, NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+  if (
+    !token &&
+    (url.pathname.startsWith("/dashboard") || url.pathname === "/") // protect root for unauthenticated users too
+  ) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/", "/sign-in", "/sign-up", "/dashboard/:path*", "/verify/:path*"],
+};
